928b460329c7b0c36fe6ba4d6eccee01c273f367
loic.haye@ipsos.com <loic.haye@ipsos.com>
2018-07-13 10:54:07 +0200

    enable http services to insert, update, delete and search heroes

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -17,6 +17,11 @@ export class HeroDetailComponent implements OnInit {
       .subscribe(hero => this.hero = hero);
   }
 
+  save(): void {
+    this.heroService.updateHero(this.hero)
+      .subscribe(() => this.goBack());
+  }
+  
   goBack(): void {
     this.location.back();
   }
@@ -31,5 +36,4 @@ export class HeroDetailComponent implements OnInit {
   ngOnInit() {
     this.getHero();
   }
-
 }
