377c3a309b4fd0cf84878e1c7c9c121270066d26
loic.haye@ipsos.com <loic.haye@ipsos.com>
2018-09-05 10:03:39 +0200

    add possibility to close message one by one

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -1,16 +1,36 @@
 import { Injectable } from '@angular/core';
+import { Message } from './messages/message';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MessageService {
 
-  messages: string[] = [];
- 
-  add(message: string) {
+  messages: Message[] = [];
+
+  generateId(): number {
+    let maxId = 0;
+    this.messages.reduce((prev: any, item: any) => {
+      maxId = Math.max(maxId, item.id);
+    }, null);
+    return maxId + 1;
+  }
+
+  add(description: string) {
+    var message: Message = {
+      id: this.generateId(),
+      description: description
+    }
     this.messages.push(message);
   }
- 
+
+  remove(message: Message) {
+     var index = this.messages.indexOf(message, 0);
+     if (index > -1) {
+       this.messages.splice(index, 1);
+     }
+  }
+
   clear() {
     this.messages = [];
   }
